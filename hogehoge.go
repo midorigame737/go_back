074d6caa64313bc7546a5c6f3a5eeeb613691d77
midorigame737 <midorigame6@gmail.com>
2022-09-01 01:55:39 +0900

    ミドルウェアテスト

--- hogehoge.go
+++ hogehoge.go
@@ -8,9 +8,15 @@ import (
 
 func main() {
 	engine := gin.Default()
+	ua := ""
+	engine.Use(func(c *gin.Context) {
+		ua = c.GetHeader("User-Agent")
+		c.Next()
+	})
 	engine.GET("/", func(c *gin.Context) { //多分.com直下にアクセスされたときの処理
 		c.JSON(http.StatusOK, gin.H{ //多分第一引数が結果、第２引数がレスポンス内容
-			"message": "hello world",
+			"message":    "hello world",
+			"User-Agent": ua,
 		})
 	})
 	engine.Run(":3000") //ポート指定
