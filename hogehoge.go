8fd5df3b211c8f81c3ac72644b1eac916c8c55be
midorigame737 <midorigame6@gmail.com>
2022-09-01 15:47:33 +0900

    ミドルウェアのところコメント追記

--- hogehoge.go
+++ hogehoge.go
@@ -8,10 +8,13 @@ import (
 
 func main() {
 	engine := gin.Default()
+	/*ginEnginの初期化
+	GET,POSTとかのエンドポイントの登録してくれる
+	*/
 	ua := ""
-	engine.Use(func(c *gin.Context) {
+	engine.Use(func(c *gin.Context) { //Useでミドルウェア登録
 		ua = c.GetHeader("User-Agent")
-		c.Next()
+		c.Next() //ここでUseで登録した関数実行
 	})
 	engine.GET("/", func(c *gin.Context) { //多分.com直下にアクセスされたときの処理
 		c.JSON(http.StatusOK, gin.H{ //多分第一引数が結果、第２引数がレスポンス内容
